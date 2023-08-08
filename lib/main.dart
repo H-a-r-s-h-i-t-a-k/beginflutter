bebbf897799feaa48181e932ba227b9829425e90
harshita <harsi@app.com>
2023-08-08 22:07:24 +0530

    third

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,29 @@ void main() {
   runApp(new MyApp());
 }
 
+// app1 start;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "First App",
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Homeeeee page"),
+//         ),
+//         body: Center(
+//           child: Text("Heyyy harshitaaaaaaaa"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// app1 end;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+// appp2 start;;;;;;;;;;;;;;;;;;;;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -16,14 +39,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "First App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Home page"),
-        ),
-        body: Center(
-          child: Text("Helllo harshita"),
-        ),
+      home: HomePage(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 233, 215, 180),
+        primarySwatch: Colors.brown,
+        brightness: Brightness.light,
+        primaryColor: Colors.amber,
       ),
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String mytext = "Hello world";
+
+  void _changeText() {
+    setState(() {
+      if (mytext.startsWith('H')) {
+        mytext = "Welcom to App";
+      } else {
+        mytext = "Hello World";
+      }
+    });
+  }
+
+  Widget _bodyWidget() {
+    return Container(
+        padding: const EdgeInsets.all(9.0),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(mytext),
+            // OutlinedButton(
+            // TextButton(
+            ElevatedButton(
+              style: TextButton.styleFrom(
+                  foregroundColor: Color.fromARGB(255, 249, 247, 248),
+                  backgroundColor: Color.fromARGB(235, 110, 130, 91)),
+              onPressed: _changeText,
+              child: const Text(
+                "click",
+                style: TextStyle(fontFamily: AutofillHints.birthdayDay),
+              ),
+              // color: Colors.deepPurpleAccent,
+            ),
+          ],
+        )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "HomePage",
+          style: TextStyle(fontSize: 23),
+        ),
+      ),
+      body: _bodyWidget(),
+      drawer: Drawer(),
+    );
+  }
+}
+
+// appp2 end;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+// appp333333333333333;;;;;;;;;;;;;;
+
+        // theme: ThemeData(
+        //   primarySwatch: Colors.pinkAccent,
+        //   brightness: Brightness.light,
+        //   accentColor: Color.red,
+        // ),
